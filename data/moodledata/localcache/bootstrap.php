fa17592015b8ccdd7f001d91d3ab75046a1f8977
gabs2678 <129786614+gabs2678@users.noreply.github.com>
2024-05-20 01:20:54 -0300

    borrando data

--- data/moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'Ttgd4K2hiNdfLX4MqiNQauZKAa9D62Kqlocalhost';
-$CFG->bootstraphash = '2a14ab2119d655a94b955764be1cae24';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
